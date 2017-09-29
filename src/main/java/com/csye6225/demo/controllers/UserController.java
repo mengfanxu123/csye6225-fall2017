5cf8b2aaf95eb713c5ce28299cadff4cff9bae06
001239511ShuangShuangXu <31753603+001239511ShuangShuangXu@users.noreply.github.com>
2017-09-29 15:43:56 -0400

    Update UserController.java

--- src/main/java/com/csye6225/demo/controllers/UserController.java
+++ src/main/java/com/csye6225/demo/controllers/UserController.java
@@ -1,5 +1,11 @@
 package com.csye6225.demo.controllers;
 
+/*
+ *  Shuangshuang Xu  001239511 xu.shua@husky.neu.edu
+ *  Mengfei Zhang    001115548 zhang.mengf@husky.neu.edu
+ *  Mengfan Xu       001238833 xu.mengf@husky.neu.edu
+ *  YeHui Rong       001957596 rong.ye@husky.neu.edu
+ */
 
 import com.csye6225.demo.auth.BCryptPasswordEncoderBean;
 import com.csye6225.demo.pojo.User;

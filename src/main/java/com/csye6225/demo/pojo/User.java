655b0b000f4754db1d90d0bcd6b97cc3e40ef7f9
001239511ShuangShuangXu <31753603+001239511ShuangShuangXu@users.noreply.github.com>
2017-09-29 15:43:30 -0400

    Update User.java

--- src/main/java/com/csye6225/demo/pojo/User.java
+++ src/main/java/com/csye6225/demo/pojo/User.java
@@ -1,5 +1,12 @@
 package com.csye6225.demo.pojo;
 
+/*
+ *  Shuangshuang Xu  001239511 xu.shua@husky.neu.edu
+ *  Mengfei Zhang    001115548 zhang.mengf@husky.neu.edu
+ *  Mengfan Xu       001238833 xu.mengf@husky.neu.edu
+ *  YeHui Rong       001957596 rong.ye@husky.neu.edu
+ */
+
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;

3b9b4c565aa5b2aab71f9029045bdbbb21c2102f
ReveRong <31931511+ReveRong@users.noreply.github.com>
2017-10-20 16:49:12 -0400

    Update DemoApplicationTests.java

--- src/test/java/com/csye6225/demo/DemoApplicationTests.java
+++ src/test/java/com/csye6225/demo/DemoApplicationTests.java
@@ -23,7 +23,7 @@ public class DemoApplicationTests {
 
 
     @Test
-    @Sql({"/test-schema.sql", "/test-user-data.sql"})
+   //@Sql({"/test-schema.sql", "/test-user-data.sql"})
     public void showAll() {
         System.out.println("Hello world");
   }

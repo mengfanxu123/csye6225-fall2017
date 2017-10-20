4d99a8d2bbbedd446f675966c1f7072b8f079a38
001239511ShuangshuangXu <xu.shua@husky.neu.edu>
2017-10-20 18:24:06 -0400

    assignment5

--- src/test/java/com/csye6225/demo/DemoApplicationTests.java
+++ src/test/java/com/csye6225/demo/DemoApplicationTests.java
@@ -15,8 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 
 public class DemoApplicationTests {
 

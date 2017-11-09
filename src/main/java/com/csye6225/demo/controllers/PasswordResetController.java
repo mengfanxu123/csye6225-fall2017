fc3fc865b1a83c65ffdadc1198e6aaf02c331dfc
001239511ShuangshuangXu <xu.shua@husky.neu.edu>
2017-11-08 23:33:40 -0500

    a8

---
+++ src/main/java/com/csye6225/demo/controllers/PasswordResetController.java
@@ -0,0 +1,26 @@
+package com.csye6225.demo.controllers;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/forget-password")
+public class PasswordResetController {
+
+    @PostMapping
+    public void resetPassword(){
+        AmazonSNSClient snsClient = new AmazonSNSClient(new ClientConfiguration());
+
+//publish to an SNS topic
+        String msg = "Reset Password Successfully";
+        String topicArn="arn:aws:sns:us-east-1:179107530530:password_reset";
+        PublishRequest publishRequest = new PublishRequest(topicArn, msg);
+        PublishResult publishResult = snsClient.publish(publishRequest);
+
+    }
+}

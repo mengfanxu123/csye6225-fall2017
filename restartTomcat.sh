3c3b79b4951f0543c095e48187ce4747d214f656
001239511ShuangshuangXu <xu.shua@husky.neu.edu>
2017-11-19 18:55:46 -0500

    a9

--- restartTomcat.sh
+++ restartTomcat.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update&&
-sudo apt-get install python -y&&
-curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O&&
-chmod +x ./awslogs-agent-setup.py&&
-sudo python ./awslogs-agent-setup.py -n -r us-east-1 -c awslogs.conf&&
 
-sudo systemctl start awslogs.service&&
+
 sudo service tomcat8 restart&&
 echo done

8a0044871b4c5292056632465acb4a975de471a3
001239511ShuangshuangXu <xu.shua@husky.neu.edu>
2017-11-29 16:10:51 -0500

    update

---
+++ infrastructure/aws/cloudformation/assignment10test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[0].VpcId" --output text)&&
+echo $VPC_ID&&
+
+SUBNET_ID1=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[0].SubnetId" --output text)&&
+echo $SUBNET_ID1&&
+
+SUBNET_ID2=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[1].SubnetId" --output text)&&
+echo $SUBNET_ID2&&
+
+SUBNET_ID3=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[2].SubnetId" --output text)&&
+echo $SUBNET_ID3&&
+
+SUBNET_ID4=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[3].SubnetId" --output text)&&
+echo $SUBNET_ID4&&
+
+
+aws cloudformation create-stack --stack-name test --template-body file://./assignmnet10.json --parameters ParameterKey=ParamVpcID,ParameterValue=${VPC_ID} ParameterKey=ParamSubnetId1,ParameterValue=${SUBNET_ID1} ParameterKey=ParamSubnetId2,ParameterValue=${SUBNET_ID2} ParameterKey=ParamSubnetId3,ParameterValue=${SUBNET_ID3} ParameterKey=ParamSubnetId4,ParameterValue=${SUBNET_ID4} --capabilities CAPABILITY_NAMED_IAM&&
+echo done
